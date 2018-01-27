f787c46097c2fb451263a8adb4c332501d8aa4d3
rickyshowtime <chautems.eric@gmail.com>
2018-01-27 03:02:26 +0100

    b6.2
    menuburger animation
    adaptative menuburger

--- js/burgermenu.js
+++ js/burgermenu.js
@@ -20,3 +20,19 @@ function setdrop2(checked) {
     }
 }
 
+$(window).on('scroll',function(){ "use strict";
+
+    // we round here to reduce a little workload
+	    // we round here to reduce a little workload
+    var stop = Math.round($(window).scrollTop());
+
+    if (stop > 100) {
+		
+		$('.navbar-toggle').addClass('loaded');
+    
+	} else {
+		
+		$('.navbar-toggle').removeClass('loaded');
+		
+	}
+});
