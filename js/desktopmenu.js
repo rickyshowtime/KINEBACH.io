8205fcc5e580a21e68a4395a95ec74ba56808e41
rickyshowtime <chautems.eric@gmail.com>
2018-01-27 01:48:20 +0100

    beta 6
    new UI for desktop (adaptative navbar)

--- js/desktopmenu.js
+++ js/desktopmenu.js
@@ -2,17 +2,33 @@
 var mainbottom = $('#section0').offset().top + $('#section0').height();
 
 // on scroll, 
-$(window).on('scroll',function(){
+$(window).on('scroll',function(){ "use strict";
 
     // we round here to reduce a little workload
     var stop = Math.round($(window).scrollTop());
 
     if (stop > mainbottom + 200) {
+		$('.nav-hide').addClass('past-main');
         $('nav li a').addClass('past-main');
         $('nav li span').addClass('past-main');
 		$('.active').addClass('past-main');
 		$('.circle').addClass('past-main');
+		$( ".hide" ).hover(function() {
+			  $(".nav-hide").addClass("roll");
+			},
+			  function() {
+			  $(".nav-hide").removeClass("roll");
+			  
+			});
+		$( "nav ul ul" ).hover(function() {
+			  $(".nav-hide").addClass("roll");
+			},
+			  function() {
+			  $(".nav-hide").removeClass("roll");
+			  
+			});
     } else {
+		$('.nav-hide').removeClass('past-main');
         $('nav li a').removeClass('past-main');
         $('nav li span').removeClass('past-main');
 		$('.active').removeClass('past-main');
@@ -21,7 +37,8 @@ $(window).on('scroll',function(){
 
 });
 
-$('a').click(function(){
+
+$('a').click(function(){ "use strict";
     $('html, body').animate({
         scrollTop: $( $(this).attr('href') ).offset().top
     }, 500);
@@ -29,7 +46,36 @@ $('a').click(function(){
 });
 
 
-$(document).ready(function() {
+$(document).ready(function() { "use strict";
     $('.nav-hide').addClass('loaded');
 	$('.section').addClass('loaded');
+});
+
+
+// get the value of the bottom of the #main element by adding the offset of that element plus its height, set it as a variable
+var mainbottom = $('#section0').offset().top + $('#section0').height();
+
+// on scroll, 
+$(window).on('scroll',function(){ "use strict";
+
+    // we round here to reduce a little workload
+    var stop = Math.round($(window).scrollTop());
+
+    if (stop < mainbottom - 450) {
+		$( ".hide" ).hover(function() {
+			  $(".nav-hide").removeClass("roll");
+			},
+			  function() {
+			  $(".nav-hide").removeClass("roll");
+			  
+			});
+		$( "nav ul ul" ).hover(function() {
+			  $(".nav-hide").removeClass("roll");
+			},
+			  function() {
+			  $(".nav-hide").removeClass("roll");
+			  
+			});
+    } 
+
 });
